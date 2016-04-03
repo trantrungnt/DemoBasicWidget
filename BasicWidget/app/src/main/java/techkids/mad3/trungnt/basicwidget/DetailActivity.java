a4e6c08705f59fc192a345ab71b0a4273cdd5175
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-04 02:50:04 +0700

    demo Basic Widget (temp)

---
+++ BasicWidget/app/src/main/java/techkids/mad3/trungnt/basicwidget/DetailActivity.java
@@ -0,0 +1,15 @@
+package techkids.mad3.trungnt.basicwidget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrungNT on 4/3/2016.
+ */
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
